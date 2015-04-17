7f3aad5140488d4da55a198be15e8ec3de99a486
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2015-04-17 16:12:58 +0300

    Merge UI with backend, use backend in CLI

---
+++ agkyra/agkyra/cli.py
@@ -0,0 +1,195 @@
+import cmd
+import sys
+import logging
+from agkyra.syncer import setup, syncer
+from agkyra.syncer.pithos_client import PithosFileClient
+from agkyra.syncer.localfs_client import LocalfsFileClient
+from agkyra import config
+# from config import AgkyraConfig
+
+
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.FileHandler('%s/agkyra.log' % config.AGKYRA_DIR))
+LOG.setLevel(logging.CRITICAL)
+
+SYNCER_LOG = logging.getLogger('agkyra.syncer')
+SYNCER_LOG.addHandler(logging.FileHandler('%s/agkyra.log' % config.AGKYRA_DIR))
+SYNCER_LOG.setLevel(logging.CRITICAL)
+
+setup.GLOBAL_SETTINGS_NAME = config.AGKYRA_DIR
+
+
+class AgkyraCLI(cmd.Cmd):
+    """The CLI for """
+
+    cnf = config.AgkyraConfig()
+    is_shell = False
+
+    def init(self):
+        """initialize syncer"""
+        # Read settings
+        sync = self.cnf.get('global', 'default_sync')
+        LOG.info('Using sync: %s' % sync)
+        cloud = self.cnf.get_sync(sync, 'cloud')
+        url = self.cnf.get_cloud(cloud, 'url')
+        token = self.cnf.get_cloud(cloud, 'token')
+        container = self.cnf.get_sync(sync, 'container')
+        directory = self.cnf.get_sync(sync, 'directory')
+
+        # Prepare syncer settings
+        self.settings = setup.SyncerSettings(
+            sync, url, token, container, directory)
+        LOG.info('Local: %s' % directory)
+        LOG.info('Remote: %s of %s' % (container, url))
+        # self.exclude = self.cnf.get_sync(sync, 'exclude')
+
+        # Init syncer
+        master = PithosFileClient(self.settings)
+        slave = LocalfsFileClient(self.settings)
+        self.syncer = syncer.FileSyncer(self.settings, master, slave)
+
+    def preloop(self):
+        """This runs when the shell loads"""
+        print 'Loading Agkyra (sometimes this takes a while)'
+        if not self.is_shell:
+            self.is_shell = True
+            self.prompt = '\xe2\x9a\x93 '
+            self.init()
+        self.default('')
+
+    def print_option(self, section, name, option):
+        """Print a configuration option"""
+        section = '%s.%s' % (section, name) if name else section
+        value = self.cnf.get(section, option)
+        print '  %s: %s' % (option, value)
+
+    def list_section(self, section, name):
+        """list contents of a section"""
+        content = dict(self.cnf.items(section))
+        if section in 'global' and name:
+            self.print_option(section, '', name)
+        else:
+            if name:
+                content = content[name]
+            for option in content.keys():
+                self.print_option(section, name, option)
+
+    def list_section_type(self, section):
+        """print the contents of a configuration section"""
+        names = ['', ] if section in ('global', ) else self.cnf.keys(section)
+        assert names, 'Section %s not found' % section
+        for name in names:
+            print section, name
+            self.list_section(section, name)
+
+    def list_sections(self):
+        """List all configuration sections"""
+        for section in self.cnf.sections():
+            self.list_section_type(section)
+
+    def do_list(self, line):
+        """List current settings (\"help list\" for details)
+        list global                 List all settings
+        list global <option>        Get the value of this global option
+        list cloud                  List all clouds
+        list cloud <name>           List all options of a cloud
+        list cloud <name> <option>  Get the value of this cloud option
+        list sync                   List all syncs
+        list sync <name>            List all options of a sync
+        list sync <name> <option>   Get the value of this sync option
+        """
+        args = line.split()
+        try:
+            {
+                0: self.list_sections,
+                1: self.list_section_type,
+                2: self.list_section,
+                3: self.print_option
+            }[len(args)](*args)
+        except Exception as e:
+            sys.stderr.write('%s\n' % e)
+            cmd.Cmd.do_help(self, 'list')
+
+    def set_global_setting(self, section, option, value):
+        assert section in ('global'), 'Syntax error'
+        self.cnf.set(section, option, value)
+
+    def set_setting(self, section, name, option, value):
+        assert section in self.sections(), 'Syntax error'
+        self.cnf.set('%s.%s' % (section, name), option, value)
+
+    def do_set(self, line):
+        """Set a setting"""
+        args = line.split()
+        try:
+            {
+                3: self.set_global_setting,
+                4: self.set_setting
+            }[len(args)](*args)
+            self.cnf.write()
+        except Exception as e:
+            sys.stderr.write('%s\n' % e)
+            cmd.Cmd.do_help(self, 'set')
+
+    def do_start(self, line):
+        """Start syncing"""
+        if not getattr(self, '_syncer_initialized', False):
+            self.syncer.probe_and_sync_all()
+            self._syncer_initialized = True
+        if self.syncer.paused:
+            self.syncer.start_decide()
+
+    def do_pause(self, line):
+        """Pause syncing"""
+        if not self.syncer.paused:
+            self.syncer.pause_decide()
+
+    def do_status(self, line):
+        """Get current status (running/paused, progress)"""
+        print 'paused' if self.syncer.paused else 'running'
+
+    # def do_shell(self, line):
+    #     """Run system, shell commands"""
+    #     if getattr(self, 'is_shell'):
+    #         os.system(line)
+    #     else:
+    #         try:
+    #             self.prompt = '\xe2\x9a\x93 '
+    #             self.is_shell = True
+    #         finally:
+    #             self.init()
+    #             self.cmdloop()
+
+    def do_help(self, line):
+        """List commands with \"help\" or detailed help with \"help cmd\""""
+        if not line:
+            self.default(line)
+        cmd.Cmd.do_help(self, line)
+
+    def do_quit(self, line):
+        """Quit Agkyra shell"""
+        return True
+
+    def default(self, line):
+        """print help"""
+        sys.stderr.write('Usage:\t%s <command> [args]\n\n' % self.prompt)
+        for arg in [c for c in self.get_names() if c.startswith('do_')]:
+            sys.stderr.write('%s\t' % arg[3:])
+            method = getattr(self, arg)
+            sys.stderr.write(method.__doc__.split('\n')[0] + '\n')
+        sys.stderr.write('\n')
+
+    def emptyline(self):
+        if not self.is_shell:
+            return self.default('')
+
+    def run_onecmd(self, argv):
+        self.prompt = argv[0]
+        self.init()
+        self.onecmd(' '.join(argv[1:]))
+
+
+# AgkyraCLI().run_onecmd(sys.argv)
+
+# or run a shell with
+# AgkyraCLI().cmdloop()
---
+++ agkyra/agkyra/protocol.py
@@ -0,0 +1,220 @@
+from ws4py.websocket import WebSocket
+import json
+import logging
+from os.path import abspath
+from agkyra.syncer import syncer
+from agkyra.config import AgkyraConfig
+from kamaki.clients.astakos import AstakosClient
+from kamaki.clients.pithos import PithosClient
+
+
+LOG = logging.getLogger(__name__)
+
+
+class WebSocketProtocol(WebSocket):
+    """Helper-side WebSocket protocol for communication with GUI:
+
+    -- INTERRNAL HANDSAKE --
+    GUI: {"method": "post", "gui_id": <GUI ID>}
+    HELPER: {"ACCEPTED": 202, "method": "post"}" or
+        "{"REJECTED": 401, "action": "post gui_id"}
+
+    -- SHUT DOWN --
+    GUI: {"method": "post", "path": "shutdown"}
+
+    -- PAUSE --
+    GUI: {"method": "post", "path": "pause"}
+    HELPER: {"OK": 200, "action": "post pause"} or error
+
+    -- start --
+    GUI: {"method": "post", "path": "start"}
+    HELPER: {"OK": 200, "action": "post start"} or error
+
+    -- GET SETTINGS --
+    GUI: {"method": "get", "path": "settings"}
+    HELPER:
+        {
+            "action": "get settings",
+            "token": <user token>,
+            "url": <auth url>,
+            "container": <container>,
+            "directory": <local directory>,
+            "exclude": <file path>
+        } or {<ERROR>: <ERROR CODE>}
+
+    -- PUT SETTINGS --
+    GUI: {
+            "method": "put", "path": "settings",
+            "token": <user token>,
+            "url": <auth url>,
+            "container": <container>,
+            "directory": <local directory>,
+            "pithos_url": <pithos URL>,
+            "exclude": <file path>
+        }
+    HELPER: {"CREATED": 201, "action": "put settings",} or
+        {<ERROR>: <ERROR CODE>, "action": "get settings",}
+
+    -- GET STATUS --
+    GUI: {"method": "get", "path": "status"}
+    HELPER: {"progress": <int>, "paused": <boolean>, "action": "get status"} or
+        {<ERROR>: <ERROR CODE>, "action": "get status"}
+    """
+
+    gui_id = None
+    accepted = False
+    settings = dict(
+        token=None, url=None,
+        container=None, directory=None,
+        exclude=None, pithos_ui=None)
+    status = dict(progress=0, paused=True)
+    file_syncer = None
+    cnf = AgkyraConfig()
+
+    def _load_settings(self):
+        sync = self.cnf.get('global', 'default_sync')
+        cloud = self.cnf.get_sync(sync, 'cloud')
+
+        url = self.cnf.get_cloud(cloud, 'url')
+        token = self.cnf.get_cloud(cloud, 'token')
+
+        astakos = AstakosClient(url, token)
+        self.settings['url'], self.settings['token'] = url, token
+
+        try:
+            endpoints = astakos.get_endpoints()['access']['serviceCatalog']
+            for endpoint in endpoints:
+                if endpoint['type'] == PithosClient.service_type:
+                    pithos_ui = endpoint['endpoints'][0]['SNF:uiURL']
+                    self.settings['pithos_ui'] = pithos_ui
+                    break
+        except Exception as e:
+            LOG.debug('Failed to retrieve pithos_ui: %s' % e)
+
+        for option in ('container', 'directory', 'exclude'):
+            self.settings[option] = self.cnf.get_sync(sync, option)
+
+    def _dump_settings(self):
+        sync = self.cnf.get('global', 'default_sync')
+        cloud = self.cnf.get_sync(sync, 'cloud')
+
+        old_url = self.cnf.get_cloud(cloud, 'url')
+        while old_url != self.settings['url']:
+            cloud = '%s_%s' % (cloud, sync)
+            try:
+                self.cnf.get_cloud(cloud, 'url')
+            except KeyError:
+                break
+
+        self.cnf.set_cloud(cloud, 'url', self.settings['url'])
+        self.cnf.set_cloud(cloud, 'token', self.settings['token'])
+        self.cnf.set_sync(sync, 'cloud', cloud)
+
+        for option in ('directory', 'container', 'exclude'):
+            self.cnf.set_sync(sync, option, self.settings[option])
+
+    # Syncer-related methods
+    def get_status(self):
+        from random import randint
+        if self.status['progress'] < 100:
+            self.status['progress'] += 0 if randint(0, 2) else 1
+        return self.status
+
+    def get_settings(self):
+        self._load_settings()
+        return self.settings
+
+    def set_settings(self, new_settings):
+        self.settings = new_settings
+        self._dump_settings()
+
+    def pause_sync(self):
+        self.status['paused'] = True
+
+    def start_sync(self):
+        self.status['paused'] = False
+
+    # WebSocket connection methods
+    def opened(self):
+        LOG.debug('Helper: connection established')
+        self._load_settings()
+
+    def closed(self, *args):
+        LOG.debug('Helper: connection closed')
+
+    def send_json(self, msg):
+        LOG.debug('send: %s' % msg)
+        self.send(json.dumps(msg))
+
+    # Protocol handling methods
+    def _post(self, r):
+        """Handle POST requests"""
+        LOG.debug('CALLED with %s' % r)
+        if self.accepted:
+            action = r['path']
+            if action == 'shutdown':
+                self.close()
+                return
+            {
+                'start': self.start_sync,
+                'pause': self.pause_sync
+            }[action]()
+            self.send_json({'OK': 200, 'action': 'post %s' % action})
+        elif r['gui_id'] == self.gui_id:
+            self.accepted = True
+            self.send_json({'ACCEPTED': 202, 'action': 'post gui_id'})
+        else:
+            action = r.get('path', 'gui_id')
+            self.send_json({'REJECTED': 401, 'action': 'post %s' % action})
+            self.terminate()
+
+    def _put(self, r):
+        """Handle PUT requests"""
+        if not self.accepted:
+            action = r['path']
+            self.send_json({'UNAUTHORIZED': 401, 'action': 'put %s' % action})
+            self.terminate()
+        else:
+            LOG.debug('put %s' % r)
+            action = r.pop('path')
+            self.set_settings(r)
+            r.update({'CREATED': 201, 'action': 'put %s' % action})
+            self.send_json(r)
+
+    def _get(self, r):
+        """Handle GET requests"""
+        action = r.pop('path')
+        if not self.accepted:
+            self.send_json({'UNAUTHORIZED': 401, 'action': 'get %s' % action})
+            self.terminate()
+        else:
+            data = {
+                'settings': self.get_settings,
+                'status': self.get_status,
+            }[action]()
+            data['action'] = 'get %s' % action
+            self.send_json(data)
+
+    def received_message(self, message):
+        """Route requests to corresponding handling methods"""
+        LOG.debug('recv: %s' % message)
+        try:
+            r = json.loads('%s' % message)
+        except ValueError as ve:
+            self.send_json({'BAD REQUEST': 400})
+            LOG.error('JSON ERROR: %s' % ve)
+            return
+        try:
+            method = r.pop('method')
+            {
+                'post': self._post,
+                'put': self._put,
+                'get': self._get
+            }[method](r)
+        except KeyError as ke:
+            self.send_json({'BAD REQUEST': 400})
+            LOG.error('KEY ERROR: %s' % ke)
+        except Exception as e:
+            self.send_json({'INTERNAL ERROR': 500})
+            LOG.error('EXCEPTION: %s' % e)
+            self.terminate()
