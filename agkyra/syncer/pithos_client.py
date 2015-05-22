a808bfc676421afae046cce7f31d0927c65ed0ee
Giorgos Korfiatis <gkorf@grnet.gr>
2015-05-22 17:16:24 +0300

    precise fetch name time stamp

--- agkyra/syncer/pithos_client.py
+++ agkyra/syncer/pithos_client.py
@@ -16,7 +16,6 @@
 from functools import wraps
 import time
 import os
-import datetime
 import threading
 import logging
 import re
@@ -100,7 +99,7 @@ class PithosSourceHandle(object):
     def register_fetch_name(self, filename):
         db = self.get_db()
         f = utils.hash_string(filename) + "_" + \
-            datetime.datetime.now().strftime("%s")
+            utils.time_stamp()
         fetch_name = utils.join_path(self.cache_fetch_name, f)
         self.fetch_name = fetch_name
         db.insert_cachename(fetch_name, self.SIGNATURE, filename)
