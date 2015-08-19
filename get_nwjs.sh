927f5f2136c61e082e0038133f9dbc905b6748f9
Giorgos Korfiatis <gkorf@grnet.gr>
2015-08-19 18:03:46 +0300

    Use newest version of nwjs

--- get_nwjs.sh
+++ get_nwjs.sh
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION="v0.12.2"
+VERSION="v0.12.3"
 
 CURPWD=$(pwd)
 cd "$(dirname "$0")"
