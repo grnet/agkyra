656b5387716be8184254334cba9388e17111edf8
Giorgos Korfiatis <gkorf@grnet.gr>
2015-10-08 17:27:34 +0300

    remove dependency on mock

--- configure.py
+++ configure.py
@@ -2,8 +2,6 @@ import os
 
 def main():
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    # this is needed for mock
-    os.system("pip install --upgrade setuptools")
 
     import get_nwjs
     get_nwjs.main()
