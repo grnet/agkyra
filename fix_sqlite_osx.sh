6a5aad775a508142fc6654ba36a046f2acdf94d6
Giorgos Korfiatis <gkorf@grnet.gr>
2015-10-21 00:15:15 +0300

    Clean up fix script

--- fix_sqlite_osx.sh
+++ fix_sqlite_osx.sh
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CURPWD=$(pwd)
 cd "$(dirname "$0")"
 
 cd dist/agkyra.app/Contents/MacOS
